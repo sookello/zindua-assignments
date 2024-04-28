85b71ef24d3095aea8750d990d2746eabf7bac75
sookello <stephenookello@gmail.com>
2024-04-29 00:38:48 +0300

    Assignment 2

--- assignment 2.py
+++ assignment 2.py
@@ -1,4 +1,4 @@
 # write a program that uses (input) to prompt a user for their name & then welcomes them
 
 name = input ('What is your name? ')
-print('Welcome' , name , 'to Zindua Coding School.')
+print('Welcome' , name , 'to Zindua School.')
