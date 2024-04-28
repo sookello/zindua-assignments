8fccce7948a16360a3daa351cc32703ec507e861
sookello <stephenookello@gmail.com>
2024-04-29 01:24:03 +0300

    2 update

---
+++ assignment3.py
@@ -0,0 +1,7 @@
+#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+
+hr=input("How many Hours have you worked? ")
+rate=input("Enter Rate per Hour: ")
+pay=float(hr)*float(rate)
+
+print("pay:",pay)
